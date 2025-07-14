ade8cfd6415693e4ce1628830aef92cffa4799fb
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-15 00:46:44 +0300

    Add files via upload

---
+++ Lectures/FirstStepsInCoding-Lab/08.PetShop/Program.cs
@@ -0,0 +1,7 @@
+﻿// 08. Pet Shop
+
+int dogs = int.Parse(Console.ReadLine());
+int cats = int.Parse(Console.ReadLine());
+double catsSum = 4;
+double dogsSum = 2.50;
+Console.WriteLine($"{((cats * catsSum) + (dogs * dogsSum))} lv.");
