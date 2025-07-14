6356605066d8b77e21748a1c97aa90ff084173ab
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-15 00:47:42 +0300

    Add files via upload

---
+++ Lectures/FirstStepsInCoding-Lab/09.YardGreening/Program.cs
@@ -0,0 +1,8 @@
+﻿// 09. Yard Greening
+
+double area = double.Parse(Console.ReadLine());
+double price = (area * 7.61) * 0.82;
+double discount = (area * 7.61) * 0.18;
+
+Console.WriteLine($"The final price is: {price}");
+Console.WriteLine($"The discount is: {discount}");
