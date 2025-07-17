d79a0d547bd7a15abd99c7149534dd342ac34aa2
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-17 18:13:23 +0300

    Add files via upload

---
+++ Exercises/FirstStepsInCoding-Exercise/09.FishTank/Program.cs
@@ -0,0 +1,11 @@
+﻿// 09. Fish Tank
+
+int length = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double percentage = double.Parse(Console.ReadLine());
+
+double volume = length * width * height;
+double volumeInLitres = volume / 1000;
+double neededLitres = volumeInLitres * (1 - (percentage/100));
+Console.WriteLine(neededLitres);
