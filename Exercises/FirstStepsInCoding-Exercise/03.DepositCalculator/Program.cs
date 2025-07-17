2ef55796b8fd70e69d0103f4f38e67a97ab05234
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-17 18:08:48 +0300

    Add files via upload

---
+++ Exercises/FirstStepsInCoding-Exercise/03.DepositCalculator/Program.cs
@@ -0,0 +1,9 @@
+﻿// 03. Deposit Calculator
+
+double depositSum = double.Parse(Console.ReadLine());
+int termofDeposit = int.Parse(Console.ReadLine());
+double annualPercentage = double.Parse(Console.ReadLine());
+
+double sum = depositSum + termofDeposit * ((depositSum * (annualPercentage/100)) / 12);
+
+Console.WriteLine(sum);
