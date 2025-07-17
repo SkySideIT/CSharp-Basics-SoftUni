fd43fe5741db0b98e02946fd346452689218e3a6
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-17 18:11:18 +0300

    Add files via upload

---
+++ Exercises/FirstStepsInCoding-Exercise/06.Repainting/Program.cs
@@ -0,0 +1,17 @@
+﻿// 06. Repainting
+
+int nailon = int.Parse(Console.ReadLine());
+int paint = int.Parse(Console.ReadLine());
+int razreditel = int.Parse(Console.ReadLine());
+int neededHours = int.Parse(Console.ReadLine());
+
+double nailonSum = (nailon + 2) * 1.50;
+double paintSum = (paint * 1.10) * 14.50;
+double razreditelSum = razreditel * 5;
+double bagsSum = 0.40;
+
+double materialSum = nailonSum + paintSum + razreditelSum + bagsSum;
+double workSum = (materialSum * 0.30) * neededHours;
+
+double result = materialSum + workSum;
+Console.WriteLine(result);
