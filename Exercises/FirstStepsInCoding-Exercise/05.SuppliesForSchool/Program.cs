718560fcec6e3ca8bc888257be11fc7882b279e6
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-17 18:10:34 +0300

    Add files via upload

---
+++ Exercises/FirstStepsInCoding-Exercise/05.SuppliesForSchool/Program.cs
@@ -0,0 +1,14 @@
+﻿// 05.Supplies for School
+
+int packofPens = int.Parse(Console.ReadLine());
+int packofMarkers =  int.Parse(Console.ReadLine());
+int litres = int.Parse(Console.ReadLine());
+double percentageDiscount = double.Parse(Console.ReadLine());
+
+double sumofPens = packofPens * 5.80;
+double sumofMarkers = packofMarkers * 7.20;
+double sumofLitres = litres * 1.20;
+
+double sum = (sumofPens + sumofMarkers + sumofLitres);
+double price = sum - (sum * (percentageDiscount/100));
+Console.WriteLine(price);
