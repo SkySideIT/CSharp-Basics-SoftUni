a8996f296a23dbb3700967e2402cacf7f477ef9f
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-17 18:09:36 +0300

    Add files via upload

---
+++ Exercises/FirstStepsInCoding-Exercise/04.VacationBooksList/Program.cs
@@ -0,0 +1,10 @@
+﻿// 04. Vacation Books List
+
+int numOfPages = int.Parse(Console.ReadLine());
+int pagesForHour = int.Parse(Console.ReadLine());
+int numOfDays = int.Parse(Console.ReadLine());
+
+int readingTime = numOfPages / pagesForHour;
+int neededHours = readingTime / numOfDays;
+
+Console.WriteLine(neededHours);
