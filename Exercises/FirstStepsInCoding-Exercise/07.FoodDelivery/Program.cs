cad4b38942a38ef4d24993f93e5953468d4c3745
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-17 18:12:03 +0300

    Add files via upload

---
+++ Exercises/FirstStepsInCoding-Exercise/07.FoodDelivery/Program.cs
@@ -0,0 +1,10 @@
+﻿// 07. Food Delivery
+
+int chickenMenuCOunt = int.Parse(Console.ReadLine());
+int fishMenuCount = int.Parse(Console.ReadLine());
+int vegetarianMenuCount =  int.Parse(Console.ReadLine());
+
+double sumMenu = chickenMenuCOunt * 10.35 + fishMenuCount * 12.40 + vegetarianMenuCount * 8.15;
+double cakeSum = sumMenu * 0.2;
+double sum = sumMenu + cakeSum + 2.50;
+Console.WriteLine(sum);
