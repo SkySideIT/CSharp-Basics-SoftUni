94c1b495ec48d09eb5232904bdcbfb5db098dc02
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-17 18:12:46 +0300

    Add files via upload

---
+++ Exercises/FirstStepsInCoding-Exercise/08.BasketballEquipment/Program.cs
@@ -0,0 +1,9 @@
+﻿// 08. Basketball Equipment
+
+int tax = int.Parse(Console.ReadLine());
+double shoes = tax * 0.60;
+double jersey = shoes * 0.80;
+double ball = jersey * 0.25;
+double accesories = ball * 0.20;
+double result = tax + (shoes + jersey + ball + accesories);
+Console.WriteLine(result);
