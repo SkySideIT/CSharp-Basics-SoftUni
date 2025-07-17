bf9a01fbb35cdaa5cdd0bd528b6485f9b5fdf4f1
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-17 18:08:01 +0300

    Add files via upload

---
+++ Exercises/FirstStepsInCoding-Exercise/02.RadiansToDegrees/Program.cs
@@ -0,0 +1,6 @@
+﻿// 02. Radians to Degrees
+
+Console.Write("Enter Radian: ");
+double radians = double.Parse(Console.ReadLine());
+double degrees = radians * (180 / Math.PI);
+Console.WriteLine("Radians to Degrees: " + degrees);
