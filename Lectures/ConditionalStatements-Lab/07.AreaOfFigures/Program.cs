df7ae11878e7188d55290b31aa33be06acf7b135
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-19 19:43:55 +0300

    Add files via upload

---
+++ Lectures/ConditionalStatements-Lab/07.AreaOfFigures/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+string figure = Console.ReadLine();
+if (figure == "square")
+{
+    double a = double.Parse(Console.ReadLine());
+    double lice = a * a;
+    Console.WriteLine($"{lice:f3}");
+}
+else if (figure == "rectangle")
+{
+    double a = double.Parse(Console.ReadLine());
+    double b = double.Parse(Console.ReadLine());
+    double lice = a * b;
+    Console.WriteLine($"{lice:f3}");
+}
+else if (figure == "circle")
+{
+    double a = double.Parse(Console.ReadLine());
+    double lice = Math.PI * (a * a);
+    Console.WriteLine($"{lice:f3}");
+}
+else if (figure == "triangle")
+{
+    double a = double.Parse(Console.ReadLine());
+    double b = double.Parse(Console.ReadLine());
+    double lice = (a * b) / 2;
+    Console.WriteLine($"{lice:f3}");
+}
