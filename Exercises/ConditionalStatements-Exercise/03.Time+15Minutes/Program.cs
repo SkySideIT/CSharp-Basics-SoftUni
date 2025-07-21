b0e53033035e13f681c6c2f26ca8bd3724a2124d
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-21 22:12:02 +0300

    Add files via upload

---
+++ Exercises/ConditionalStatements-Exercise/03.Time+15Minutes/Program.cs
@@ -0,0 +1,24 @@
+﻿int hour = int.Parse(Console.ReadLine());
+int minutes = int.Parse(Console.ReadLine());
+int newTime = minutes + 15;
+if (newTime > 59)
+{
+    hour += 1;
+    minutes = newTime - 60;
+}
+else
+{
+    minutes = newTime;
+}
+if (hour > 23)
+{
+    hour = 0;
+}
+if (minutes < 10)
+{
+    Console.WriteLine($"{hour}:0{minutes}");
+}
+else
+{
+    Console.WriteLine($"{hour}:{minutes}");
+}
