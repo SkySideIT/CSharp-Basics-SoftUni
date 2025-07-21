c99148c08bd817196cfe69b424ba02cc2beec9b2
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-21 22:14:05 +0300

    Add files via upload

---
+++ Exercises/ConditionalStatements-Exercise/06.WorldSwimmingRecord/Program.cs
@@ -0,0 +1,16 @@
+﻿double record = double.Parse(Console.ReadLine());
+double metres  = double.Parse(Console.ReadLine());
+double secFor1metre = double.Parse(Console.ReadLine());
+
+double secondsNeeded = metres * secFor1metre;
+double delaySeconds = Math.Floor(metres / 15) * 12.5;
+double sumTime = secondsNeeded + delaySeconds;
+if (sumTime < record)
+{
+    Console.WriteLine($"Yes, he succeeded! The new world record is {sumTime:f2} seconds.");
+}
+else
+{
+    double neededSecond = sumTime - record;
+    Console.WriteLine($"No, he failed! He was {neededSecond:f2} seconds slower.");
+}
