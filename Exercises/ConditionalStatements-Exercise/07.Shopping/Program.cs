e27e2bcbd2ce0330230a40e5df42b39b2c42d421
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-21 22:14:52 +0300

    Add files via upload

---
+++ Exercises/ConditionalStatements-Exercise/07.Shopping/Program.cs
@@ -0,0 +1,29 @@
+﻿double budget = double.Parse(Console.ReadLine());
+int numGPUs = int.Parse(Console.ReadLine());
+int numCPUs  = int.Parse(Console.ReadLine());
+int numRAMs =  int.Parse(Console.ReadLine());
+
+double gpuCost = numGPUs * 250;
+double cpuCost = numCPUs * (gpuCost * 0.35);
+double ramCost = numRAMs * (gpuCost * 0.10);
+
+double allInAll = 0.0;
+if (numGPUs > numCPUs)
+{
+    allInAll = (gpuCost + cpuCost + ramCost) * 0.85;
+}
+else
+{
+    allInAll = gpuCost + cpuCost + ramCost;
+}
+
+if (budget < allInAll)
+{
+    double needed = allInAll - budget;
+    Console.WriteLine($"Not enough money! You need {needed:f2} leva more!");
+}
+else
+{
+    double left = budget - allInAll;
+    Console.WriteLine($"You have {left:f2} leva left!");
+}
