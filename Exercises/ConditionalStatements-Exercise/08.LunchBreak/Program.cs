d0e3ddc3fa4559f1471fdb5f2650fb364caca8c8
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-21 22:15:32 +0300

    Add files via upload

---
+++ Exercises/ConditionalStatements-Exercise/08.LunchBreak/Program.cs
@@ -0,0 +1,16 @@
+﻿string name = Console.ReadLine();
+int playTime = int.Parse(Console.ReadLine());
+int pauseTime = int.Parse(Console.ReadLine());
+
+double timeForLunch = pauseTime * 0.125;
+double timeForRest = pauseTime * 0.25;
+double timeLeft = pauseTime - (timeForLunch + timeForRest);
+
+if (timeLeft >= playTime)
+{
+    Console.WriteLine($"You have enough time to watch {name} and left with {Math.Ceiling(timeLeft - playTime)} minutes free time.");
+}
+else
+{
+    Console.WriteLine($"You don't have enough time to watch {name}, you need {Math.Ceiling(playTime - timeLeft)} more minutes.");
+}
