ed5666e91d1deaa736b8c7a6f03e14d84c0ff70d
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-21 22:13:25 +0300

    Add files via upload

---
+++ Exercises/ConditionalStatements-Exercise/05.GodzillaVsKong/Program.cs
@@ -0,0 +1,21 @@
+﻿double budget = double.Parse(Console.ReadLine());
+int numStatists = int.Parse(Console.ReadLine());
+double clothesForOne =  double.Parse(Console.ReadLine());
+
+double decor = budget * 0.10;
+if (numStatists > 150)
+{
+    clothesForOne = clothesForOne * 0.90; 
+}
+double clothesAll = numStatists * clothesForOne;
+double sum = decor + clothesAll;
+if (sum > budget)
+{
+    Console.WriteLine("Not enough money!");
+    Console.WriteLine($"Wingard needs {(sum - budget):f2} leva more.");
+}
+else
+{
+    Console.WriteLine("Action!");
+    Console.WriteLine($"Wingard starts filming with {(budget - sum):f2} leva left.");
+}
