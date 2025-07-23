3d8198a5f4d45b53a9c61dc15052872af7a7f35e
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-23 17:17:39 +0300

    Add files via upload

---
+++ Lectures/ConditionalStatementsAdvanced-Lab/08.CinemaTicket/Program.cs
@@ -0,0 +1,18 @@
+﻿string day = Console.ReadLine();
+
+double price = 0;
+
+if (day == "Monday" || day == "Tuesday" || day == "Friday")
+{
+    price = 12;
+}
+else if (day == "Wednesday" || day == "Thursday")
+{
+    price = 14;
+}
+else if (day == "Saturday" || day == "Sunday")
+{
+    price = 16;
+}
+
+Console.WriteLine(price);
