b2526f8df8435e3d4f2b41e653bb2efa030cd943
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-25 23:24:42 +0300

    Add files via upload

---
+++ Exercises/ConditionalStatementsAdvanced-Exercise/01.Cinema/Program.cs
@@ -0,0 +1,22 @@
+﻿string type = Console.ReadLine();
+int numRows = int.Parse(Console.ReadLine());
+int numCols = int.Parse(Console.ReadLine());
+
+double seats = numCols * numRows;
+double sum = 0;
+
+if (type == "Premiere")
+{
+    sum = seats * 12.00;
+    Console.WriteLine($"{sum:f2} leva");
+}
+else if (type == "Normal")
+{
+    sum = seats * 7.50;
+    Console.WriteLine($"{sum:f2} leva");
+}
+else if (type == "Discount")
+{
+    sum = seats * 5.00;
+    Console.WriteLine($"{sum:f2} leva");
+}
