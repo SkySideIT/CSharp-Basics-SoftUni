688c377cb0149081089b883071578426019ca130
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-25 23:26:46 +0300

    Add files via upload

---
+++ Exercises/ConditionalStatementsAdvanced-Exercise/05.Journey/Program.cs
@@ -0,0 +1,44 @@
+﻿double budget = double.Parse(Console.ReadLine());
+string season =  Console.ReadLine();
+
+double cost = 0;
+string destination = "";
+string type = "";
+
+if (budget <= 100)
+{
+    destination = "Bulgaria";
+    if (season == "summer")
+	{
+        type = "Camp";
+		cost = budget * 0.30;
+	}
+	else if(season == "winter")
+	{
+        type = "Hotel";
+		cost = budget * 0.70;
+	}
+}
+else if (budget <= 1000)
+{
+    destination = "Balkans";
+    if (season == "summer")
+    {
+        type = "Camp";
+        cost = budget * 0.40;
+    }
+    else if (season == "winter")
+    {
+        type = "Hotel";
+        cost = budget * 0.80;
+    }
+}
+else if (budget > 1000)
+{
+    destination = "Europe";
+    type = "Hotel";
+    cost = budget * 0.90;
+}
+
+Console.WriteLine($"Somewhere in {destination}");
+Console.WriteLine($"{type} - {cost:f2}");
