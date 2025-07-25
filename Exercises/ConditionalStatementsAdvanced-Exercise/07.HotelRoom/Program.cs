7e81a6ac589c3d3eb65924f8d1becddb30a5c22a
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-25 23:28:06 +0300

    Add files via upload

---
+++ Exercises/ConditionalStatementsAdvanced-Exercise/07.HotelRoom/Program.cs
@@ -0,0 +1,41 @@
+﻿string month = Console.ReadLine();
+int nights = int.Parse(Console.ReadLine());
+
+double studio = 0;
+double apartment = 0;
+
+if (month == "May" || month == "October")
+{
+	studio = nights * 50;
+	apartment = nights * 65;
+    if (nights > 7 && nights <= 14)
+    {
+        studio = studio * 0.95;
+    }
+    else if (nights > 14) 
+    {
+        studio = studio * 0.70;
+        apartment = apartment * 0.90;
+    }
+}
+else if (month == "June" || month == "September")
+{
+    studio = nights * 75.20;
+    apartment = nights * 68.70;
+    if (nights > 14)
+    {
+        apartment = apartment * 0.90;
+    }
+}
+else if (month == "July" || month == "August")
+{
+    studio = nights * 76;
+    apartment = nights * 77;
+    if (nights > 14)
+    {
+        apartment = apartment * 0.90;
+    }
+}
+
+Console.WriteLine($"Apartment: {apartment:f2} lv.");
+Console.WriteLine($"Studio: {studio:f2} lv.");
