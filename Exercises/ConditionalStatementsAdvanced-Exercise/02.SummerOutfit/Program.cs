2d800a4f7d9587cfb805fbd4e4c979eb717f89c1
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-25 23:25:33 +0300

    Add files via upload

---
+++ Exercises/ConditionalStatementsAdvanced-Exercise/02.SummerOutfit/Program.cs
@@ -0,0 +1,98 @@
+﻿int degrees = int.Parse(Console.ReadLine());
+string timeOfDay = Console.ReadLine();
+string outfit = "";
+string shoes = "";
+
+if (degrees >= 10 && degrees <= 18)
+{
+    //if (timeOfDay == "Morning")
+    //{
+    //    outfit = "Sweatshirt";
+    //    shoes = "Sneakers";
+    //}
+    //else if (timeOfDay == "Afternoon" || timeOfDay == "Evening")
+    //{
+    //    outfit = "Shirt";
+    //    shoes = "Moccasins";
+    //}
+
+    switch (timeOfDay)
+    {
+        case "Morning":
+            outfit = "Sweatshirt";
+            shoes = "Sneakers";
+            break;
+        case "Afternoon":
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            break;
+        default:
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            break;
+    }
+}
+else if (degrees > 18 && degrees <= 24)
+{
+    //if (timeOfDay == "Morning" || timeOfDay == "Evening")
+    //{
+    //    outfit = "Shirt";
+    //    shoes = "Moccasins";
+    //}
+    //else if (timeOfDay == "Afternoon")
+    //{
+    //    outfit = "T-Shirt";
+    //    shoes = "Sandals";
+    //}
+
+    switch (timeOfDay)
+    {
+        case "Morning":
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            break;
+        case "Afternoon":
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+            break;
+        default:
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            break;
+    }
+}
+else if (degrees > 24 && degrees <= 42)
+{
+    //if (timeOfDay == "Morning")
+    //{
+    //    outfit = "T-Shirt";
+    //    shoes = "Sandals";
+    //}
+    //else if (timeOfDay == "Afternoon")
+    //{
+    //    outfit = "Swim Suit";
+    //    shoes = "Barefoot";
+    //}
+    //else if (timeOfDay == "Evening")
+    //{
+    //    outfit = "Shirt";
+    //    shoes = "Moccasins";
+    //}
+
+    switch (timeOfDay)
+    {
+        case "Morning":
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+            break;
+        case "Afternoon":
+            outfit = "Swim Suit";
+            shoes = "Barefoot";
+            break;
+        default:
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            break;
+    }
+}
+Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
