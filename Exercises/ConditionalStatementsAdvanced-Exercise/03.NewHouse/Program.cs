79081b033554a77325a0442e5e7375117b85e1fe
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-25 23:25:58 +0300

    Add files via upload

---
+++ Exercises/ConditionalStatementsAdvanced-Exercise/03.NewHouse/Program.cs
@@ -0,0 +1,55 @@
+﻿string flowers = Console.ReadLine();
+int number = int.Parse(Console.ReadLine());
+int budget = int.Parse(Console.ReadLine());
+
+double sum = 0;
+
+if (flowers == "Roses")
+{
+    sum = number * 5.00;
+	if (number > 80)
+	{
+		sum = sum * 0.90;
+	}
+}
+else if (flowers == "Dahlias")
+{
+	sum = number * 3.80;
+    if (number > 90)
+    {
+        sum = sum * 0.85;
+    }
+}
+else if (flowers == "Tulips")
+{
+    sum = number * 2.80;
+    if (number > 80)
+    {
+        sum = sum * 0.85;
+    }
+}
+else if (flowers == "Narcissus")
+{
+    sum = number * 3.00;
+    if (number < 120)
+    {
+        sum = sum * 1.15;
+    }
+}
+else if (flowers == "Gladiolus")
+{
+    sum = number * 2.50;
+    if (number < 80)
+    {
+        sum = sum * 1.20;
+    }
+}
+
+if (sum <= budget)
+{
+    Console.WriteLine($"Hey, you have a great garden with {number} {flowers} and {budget-sum:f2} leva left.");
+}
+else
+{
+    Console.WriteLine($"Not enough money, you need {sum-budget:f2} leva more.");
+}
