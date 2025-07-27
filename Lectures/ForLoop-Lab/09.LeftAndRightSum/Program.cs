8510fbb33e6ec9f1f40ad179f06cd6ea40f49474
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-27 22:10:00 +0300

    Add files via upload

---
+++ Lectures/ForLoop-Lab/09.LeftAndRightSum/Program.cs
@@ -0,0 +1,24 @@
+﻿int times = int.Parse(Console.ReadLine());
+
+int leftSum =0;
+for (int i = 0; i < times; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+    leftSum += num;
+}
+
+int rightSum =0;
+for(int i = 0;i < times; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+    rightSum += num;
+}
+
+if (leftSum == rightSum)
+{
+    Console.WriteLine($"Yes, sum = {leftSum}");
+}
+else
+{
+    Console.WriteLine($"No, diff = {Math.Abs(leftSum - rightSum)}");
+}
