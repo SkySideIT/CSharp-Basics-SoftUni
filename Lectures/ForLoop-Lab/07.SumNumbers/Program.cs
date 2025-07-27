9b1a3e6fbff3878cad93bb8a15fb71d46b8ff3ca
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-27 22:08:50 +0300

    Add files via upload

---
+++ Lectures/ForLoop-Lab/07.SumNumbers/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+int nums = int.Parse(Console.ReadLine());
+
+int sum = 0;
+for (int i = 0; i < nums; i++)
+{
+    int numN = int.Parse(Console.ReadLine());
+    sum += numN;
+}
+Console.WriteLine(sum);
