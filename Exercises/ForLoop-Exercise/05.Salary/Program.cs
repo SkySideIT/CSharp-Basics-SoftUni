090fd908fb948343c9ea5a556e1a80d3461baf72
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-29 22:37:47 +0300

    Add files via upload

---
+++ Exercises/ForLoop-Exercise/05.Salary/Program.cs
@@ -0,0 +1,29 @@
+﻿int tabs = int.Parse(Console.ReadLine());
+int salary = int.Parse(Console.ReadLine());
+for (int i = 0; i < tabs; i++)
+{
+	string tabName = Console.ReadLine();
+	if (tabName == "Facebook")
+	{
+		salary -= 150;
+	}
+	else if (tabName == "Instagram")
+    {
+        salary -= 100;
+    }
+    else if (tabName == "Reddit")
+    {
+        salary -= 50;
+    }
+
+    if (salary <= 0)
+    {
+        Console.WriteLine("You have lost your salary.");
+        break;
+    }
+}
+
+if (salary > 0)
+{
+    Console.WriteLine(salary);
+}
