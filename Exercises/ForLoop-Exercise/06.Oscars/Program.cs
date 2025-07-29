7df3ddbf060f8a005cd496970b0f5da4ca9fcc14
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-07-29 22:38:24 +0300

    Add files via upload

---
+++ Exercises/ForLoop-Exercise/06.Oscars/Program.cs
@@ -0,0 +1,22 @@
+﻿string nameOfActor = Console.ReadLine();
+double points = double.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string pointsGiver = Console.ReadLine();
+    double pointFromGiver = double.Parse(Console.ReadLine());
+
+    points += ((pointsGiver.Length * pointFromGiver) / 2);
+
+    if(points >= 1250.5)
+{
+        Console.WriteLine($"Congratulations, {nameOfActor} got a nominee for leading role with {points:f1}!");
+        break;
+    }
+}
+
+if (points < 1250.5)
+{
+    Console.WriteLine($"Sorry, {nameOfActor} you need {1250.5 - points:f1} more!");
+}
