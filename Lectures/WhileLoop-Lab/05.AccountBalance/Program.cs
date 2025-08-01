119a517f922ed6c1d04fb6726fb96688a10bb420
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-01 19:22:54 +0300

    Add files via upload

---
+++ Lectures/WhileLoop-Lab/05.AccountBalance/Program.cs
@@ -0,0 +1,35 @@
+﻿//string putIn = Console.ReadLine();
+//double sum = 0;
+
+//while (putIn != "NoMoreMoney")
+//{
+//    Console.WriteLine($"Increase: {double.Parse(putIn):f2}");
+//    sum += double.Parse(putIn);
+//    putIn = Console.ReadLine();
+//    if (double.Parse(putIn) < 0)
+//    {
+//        Console.WriteLine("Invalid operation!");
+//        break;
+//    }
+//}
+//Console.WriteLine($"Total: {sum:f2}");
+
+string input = Console.ReadLine();
+
+double balance = 0;
+while (input != "NoMoreMoney")
+{
+    double amount = double.Parse(input);
+
+    if (amount < 0)
+    {
+        Console.WriteLine("Invalid operation!");
+        break;
+    }
+
+    Console.WriteLine($"Increase: {amount:f2}");
+    balance += amount;
+
+    input = Console.ReadLine();
+}
+Console.WriteLine($"Total: {balance:f2}");
