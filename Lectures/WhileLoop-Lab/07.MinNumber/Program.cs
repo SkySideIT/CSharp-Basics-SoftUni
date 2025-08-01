ab739307b2fa94b5038c06ab1fdaaa529e15fbb5
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-01 19:24:37 +0300

    Add files via upload

---
+++ Lectures/WhileLoop-Lab/07.MinNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿string input = Console.ReadLine();
+double minValue = double.MaxValue;
+
+while (input != "Stop")
+{
+    double num = double.Parse(input);
+
+    if (num < minValue)
+    {
+        minValue = num;
+    }
+
+    input = Console.ReadLine();
+}
+Console.WriteLine(minValue);
