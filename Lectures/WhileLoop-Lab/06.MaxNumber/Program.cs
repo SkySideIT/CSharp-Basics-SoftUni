fd8463464efdb3f201315a7d7f0c9474b8cecf92
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-01 19:23:30 +0300

    Add files via upload

---
+++ Lectures/WhileLoop-Lab/06.MaxNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿string input = Console.ReadLine();
+double maxValue = double.MinValue;
+
+while (input != "Stop")
+{
+    double num = double.Parse(input);
+
+    if (num > maxValue)
+    {
+        maxValue = num;
+    }
+
+    input = Console.ReadLine();
+}
+Console.WriteLine(maxValue);
