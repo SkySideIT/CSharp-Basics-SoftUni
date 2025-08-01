2ca8bdec4e6901bcb197a575def6c69df5266ff4
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-01 19:25:15 +0300

    Add files via upload

---
+++ Lectures/WhileLoop-Lab/08.Graduation/Program.cs
@@ -0,0 +1,55 @@
+﻿//string name = Console.ReadLine();
+//double grade = double.Parse(Console.ReadLine());
+//int year = 1;
+//double sum = 0.0;
+//int count = 0;
+
+//while (year <= 12)
+//{
+//    grade = double.Parse(Console.ReadLine());
+//	if (grade < 4.00)
+//	{
+//		count++;
+//	}
+//	else
+//	{
+//		sum = sum + grade;
+//		year++;
+//	}
+
+//	if (count == 2)
+//	{
+//		Console.WriteLine($"{name} has been excluded at {year} grade");
+//		break;
+//	}
+//}
+//Console.WriteLine($"{name} graduated. Average grade: {(sum / 12):f2}");
+
+string name = Console.ReadLine();
+
+int year = 1;
+int failures = 0;
+double sum = 0;
+
+while (year <= 12 && failures < 2)
+{
+    double grade = double.Parse(Console.ReadLine());
+	if (grade >= 4)
+	{
+		sum += grade;
+		year++;
+	}
+	else
+	{
+		failures++;
+	}
+}
+
+if (failures == 2)
+{
+    Console.WriteLine($"{name} has been excluded at {year} grade");
+}
+else
+{
+    Console.WriteLine($"{name} graduated. Average grade: {(sum / 12):f2}");
+}
