ab8ee3b2bbea8dbe286c053d53b8f98d2949c59a
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-02 22:29:51 +0300

    Add files via upload

---
+++ Exercises/WhileLoop-Exercise/06.Cake/Program.cs
@@ -0,0 +1,19 @@
+﻿int height = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int piecesNumber = height * width;
+string input = Console.ReadLine();
+
+while (input != "STOP" || piecesNumber <= 0)
+{
+    piecesNumber -= int.Parse(input);
+    if (piecesNumber < 0)
+    {
+        Console.WriteLine($"No more cake left! You need {Math.Abs(piecesNumber)} pieces more.");
+        break;
+    }
+    input = Console.ReadLine();
+}
+if (input == "STOP")
+{
+    Console.WriteLine($"{piecesNumber} pieces are left.");
+}
