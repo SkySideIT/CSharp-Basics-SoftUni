dfddcef2dc0008226585c6646fc98f751bafd989
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-02 22:26:48 +0300

    Add files via upload

---
+++ Exercises/WhileLoop-Exercise/01.OldBooks/Program.cs
@@ -0,0 +1,20 @@
+﻿string book = Console.ReadLine();
+string guess = Console.ReadLine();
+int count = 0;
+
+while (guess != book)
+{
+    guess = Console.ReadLine();
+    count++;
+    if (guess == "No More Books")
+    {
+        Console.WriteLine($"The book you search is not here!");
+        Console.WriteLine($"You checked {count} books.");
+        break;
+    }
+}
+
+if (guess == book)
+{
+    Console.WriteLine($"You checked {count} books and found it.");
+}
