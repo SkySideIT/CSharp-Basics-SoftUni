9a7b27133956d3d94841d133827c229ae270e963
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-02 22:28:41 +0300

    Add files via upload

---
+++ Exercises/WhileLoop-Exercise/04.Walking/Program.cs
@@ -0,0 +1,22 @@
+﻿int sumSteps = 0;
+
+while (sumSteps < 10000)
+{
+    string steps = Console.ReadLine();
+    if (steps == "Going home")
+    {
+        sumSteps += int.Parse(Console.ReadLine());
+        break;
+    }
+    sumSteps += int.Parse(steps);
+}
+
+if (sumSteps >= 10000)
+{
+    Console.WriteLine("Goal reached! Good job!");
+    Console.WriteLine($"{sumSteps - 10000} steps over the goal!");
+}
+else
+{
+    Console.WriteLine($"{10000 - sumSteps} more steps to reach goal.");
+}
