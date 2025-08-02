e3baca087b4e49dabd9ba348825a018ab0f8eade
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-02 22:30:19 +0300

    Add files via upload

---
+++ Exercises/WhileLoop-Exercise/07.Moving/Program.cs
@@ -0,0 +1,20 @@
+﻿int width = int.Parse(Console.ReadLine());
+int lenght = int.Parse(Console.ReadLine());
+int height =  int.Parse(Console.ReadLine());
+int volume = width * height * lenght;
+string input  = Console.ReadLine();
+
+while (input != "Done")
+{
+    volume -= int.Parse(input);
+	if (volume < 0)
+	{
+        Console.WriteLine($"No more free space! You need {Math.Abs(volume)} Cubic meters more.");
+        break;
+    }
+    input = Console.ReadLine();
+}
+if (input == "Done")
+{
+    Console.WriteLine($"{volume} Cubic meters left.");
+}
