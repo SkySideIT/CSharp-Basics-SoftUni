f17597d0ffb7395cd71531eb48e3946b3cbf1c68
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-06 23:07:12 +0300

    Add files via upload

---
+++ Lectures/NestedLoops-Lab/04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿int start = int.Parse(Console.ReadLine());
+int end  = int.Parse(Console.ReadLine());
+int target = int.Parse(Console.ReadLine());
+
+bool sumIsFound = false;
+int count = 0;
+
+for (int i = start; i <= end; i++)
+{
+    for (int y = start; y <= end; y++)
+    {
+        count++;
+        if (i + y == target)
+        {
+            Console.WriteLine($"Combination N:{count} ({i} + {y} = {target})");
+            sumIsFound = true;
+            break;
+        }
+    }
+
+    if (sumIsFound)
+    {
+        break;
+    }
+}
+
+if (!sumIsFound)
+{
+    Console.WriteLine($"{count} combinations - neither equals {target}");
+}
