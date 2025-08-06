607dde19fd3e548315badc191d6c27b95160387b
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-06 23:06:34 +0300

    Add files via upload

---
+++ Lectures/NestedLoops-Lab/03.Combinations/Program.cs
@@ -0,0 +1,18 @@
+﻿int number = int.Parse(Console.ReadLine());
+int count = 0;
+
+for (int i = 0; i <= number; i++)
+{
+	for (int y = 0; y <= number; y++)
+	{
+		for (int z = 0; z <= number; z++)
+		{
+			if (i + y + z == number)
+			{
+				count++;
+			}
+		}
+	}
+}
+
+Console.WriteLine(count);
