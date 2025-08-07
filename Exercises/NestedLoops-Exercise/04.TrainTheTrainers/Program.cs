95aa37a51ba5ff7c88844358986ab869f3e6e9b6
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-07 23:42:01 +0300

    Add files via upload

---
+++ Exercises/NestedLoops-Exercise/04.TrainTheTrainers/Program.cs
@@ -0,0 +1,25 @@
+﻿int people = int.Parse(Console.ReadLine());
+string presentationName = Console.ReadLine();
+
+double sum = 0;
+double sumAll = 0;
+double count = 0;
+
+while (presentationName != "Finish")
+{
+    for (int i = 0; i < people; i++)
+    {
+        double grade = double.Parse(Console.ReadLine());
+        count++;
+        sum += grade;
+        sumAll += grade;
+    }
+    Console.WriteLine($"{presentationName} - {(sum / people):f2}.");
+    sum = 0;
+    presentationName = Console.ReadLine();
+}
+
+if (presentationName == "Finish")
+{
+    Console.WriteLine($"Student's final assessment is {(sumAll / count):f2}.");
+}
