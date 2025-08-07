3352be6984bd17225fc934c663ab3c3ef85d1d6d
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-07 23:40:03 +0300

    Add files via upload

---
+++ Exercises/NestedLoops-Exercise/01.NumberPyramid/Program.cs
@@ -0,0 +1,22 @@
+﻿int number = int.Parse(Console.ReadLine());
+int currentNum = 1;
+bool isEnded = false;
+
+for (int i = 1; i <= number; i++)
+{
+	for (int j = 1; j <= i; j++)
+	{
+        Console.Write($"{currentNum} ");
+        currentNum++;
+        if (currentNum > number)
+        {
+            isEnded = true;
+            break;
+        }
+    }
+    Console.WriteLine();
+    if (isEnded)
+    {
+        break;
+    }
+}
