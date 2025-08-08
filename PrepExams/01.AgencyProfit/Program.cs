cb0ca216442d0225621348b2fb131b97746fa926
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-08 22:19:24 +0300

    Add files via upload

---
+++ PrepExams/01.AgencyProfit/Program.cs
@@ -0,0 +1,11 @@
+﻿string companyName = Console.ReadLine();
+int adultTickectCount = int.Parse(Console.ReadLine());
+int childTickectCount = int.Parse(Console.ReadLine());
+double adultTicketPrice = double.Parse(Console.ReadLine());
+double serviceFee = double.Parse(Console.ReadLine());
+
+double childTickedPrice = 0.30 * adultTicketPrice;
+double totalPrice = (adultTickectCount * (adultTicketPrice + serviceFee)) + (childTickectCount * (childTickedPrice + serviceFee));
+double agencyProfit = totalPrice * 0.20;
+
+Console.WriteLine($"The profit of your agency from {companyName} tickets is {agencyProfit:f2} lv.");
