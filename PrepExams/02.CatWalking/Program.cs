c814625e30360de7c381bb324519b705d97662ed
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-08 22:20:05 +0300

    Add files via upload

---
+++ PrepExams/02.CatWalking/Program.cs
@@ -0,0 +1,18 @@
+﻿int minutesWalk = int.Parse(Console.ReadLine());
+int numberOfWalks = int.Parse(Console.ReadLine());
+int calories = int.Parse(Console.ReadLine());
+
+int totalMinutesWalk = minutesWalk * numberOfWalks;
+int totalCaloriesBurned =  5 * totalMinutesWalk;
+int halfCalories =  calories / 2;
+
+bool isEnough = totalCaloriesBurned * 2 >= calories;
+
+if (isEnough)
+{
+    Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {totalCaloriesBurned}.");
+}
+else
+{
+    Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {totalCaloriesBurned}.");
+}
