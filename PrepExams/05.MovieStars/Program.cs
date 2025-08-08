74de81118f4a4087947ae70cdf015f2cacfb7e16
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-08 22:22:04 +0300

    Add files via upload

---
+++ PrepExams/05.MovieStars/Program.cs
@@ -0,0 +1,26 @@
+﻿double budgetForActors = double.Parse(Console.ReadLine());
+string input = Console.ReadLine();
+
+while (input != "ACTION")
+{
+	if (input.Length <= 15)
+	{
+		double payCheck = double.Parse(Console.ReadLine());
+		budgetForActors -= payCheck;
+		if (budgetForActors < 0)
+		{
+            Console.WriteLine($"We need {Math.Abs(budgetForActors):f2} leva for our actors.");
+			break;
+        }
+	}
+	else 
+	{
+		budgetForActors *= 0.80;
+	}
+	input = Console.ReadLine();
+}
+
+if (input == "ACTION")
+{
+    Console.WriteLine($"We are left with {budgetForActors:f2} leva.");
+}
