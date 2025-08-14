0c5f21bea742f2b1cb0052cb445b97d935f6d102
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-14 14:27:14 +0300

    Add files via upload

---
+++ Exam/04.GrandpaStavri/Program.cs
@@ -0,0 +1,28 @@
+﻿int days = int.Parse(Console.ReadLine());
+
+double totalLitres = 0;
+double totalDegrees = 0;
+
+for (int i = 0; i < days; i++)
+{
+    double litres = double.Parse(Console.ReadLine());
+    totalLitres += litres;
+    double degrees = double.Parse(Console.ReadLine());
+    totalDegrees += litres * degrees;
+}
+
+double totalDegreesAverage = totalDegrees / totalLitres;
+Console.WriteLine($"Liter: {totalLitres:f2}");
+Console.WriteLine($"Degrees: {totalDegreesAverage:f2}");
+if (totalDegreesAverage < 38)
+{
+    Console.WriteLine($"Not good, you should baking!");
+}
+else if (totalDegreesAverage > 38 && totalDegreesAverage < 42)
+{
+    Console.WriteLine($"Super!");
+}
+else
+{
+    Console.WriteLine($"Dilution with distilled water!");
+}
