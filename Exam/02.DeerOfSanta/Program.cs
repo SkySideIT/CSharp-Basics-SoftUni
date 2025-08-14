f903154063504eed4a95d818340d2ce660512ae4
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-14 14:25:41 +0300

    Add files via upload

---
+++ Exam/02.DeerOfSanta/Program.cs
@@ -0,0 +1,19 @@
+﻿int days = int.Parse(Console.ReadLine());
+int foodInKilos = int.Parse(Console.ReadLine());
+double foodForFirst = double.Parse(Console.ReadLine());
+double foodForSecond = double.Parse(Console.ReadLine());
+double foodForThird = double.Parse(Console.ReadLine());
+
+double first = days * foodForFirst;
+double second = days * foodForSecond;
+double third = days * foodForThird;
+double totalFoodNeeded = first + second + third;
+
+if (foodInKilos > totalFoodNeeded)
+{
+    Console.WriteLine($"{Math.Floor(foodInKilos - totalFoodNeeded)} kilos of food left.");
+}
+else
+{
+    Console.WriteLine($"{Math.Ceiling(totalFoodNeeded - foodInKilos)} more kilos of food are needed.");
+}
