9472a4fd2ddf82b05171776eb191524c2fe14c61
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-14 14:25:09 +0300

    Add files via upload

---
+++ Exam/01.CatDiet/Program.cs
@@ -0,0 +1,14 @@
+﻿double fats = double.Parse(Console.ReadLine());
+double proteins = double.Parse(Console.ReadLine());
+double carbohydrates = double.Parse(Console.ReadLine());
+double totalCalories = double.Parse(Console.ReadLine());
+double water = double.Parse(Console.ReadLine());
+
+double totalFatGrams = (totalCalories * (fats / 100)) / 9;
+double totalProteinGrams = (totalCalories * (proteins / 100)) / 4;
+double totalCarboGrams = (totalCalories * (carbohydrates / 100)) / 4;
+
+double foodWeight = totalCarboGrams + totalFatGrams + totalProteinGrams;
+double caloriesPerGram = totalCalories / foodWeight;
+double realCalories = ((100 - water) / 100) * caloriesPerGram;
+Console.WriteLine($"{realCalories:f4}");
